1cde17c2132518ba58b034be24b75a173bbbc587
Daofeng Li <lidaof@gmail.com>
2018-01-31 16:59:14 -0600

    first commit

---
+++ multi-view/src/App.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '', data:null };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    componentWillMount() {
+      axios
+      .get('/report/GM-AM-6S-GM-172_S1_L007_R1_001_report.txt,GM-AM-6S-GM-174_S3_L007_R1_001_report.txt,GM-AM-6S-GM-173_S2_L007_R1_001_report.txt')
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => console.log(err))
+  }
+
+   render() {
+     //console.log(ReactD3);
+     //console.log(this.state);
+    return (
+      <div>
+        
+    <form onSubmit={this.handleSubmit}>
+      <label>
+        Choose your samples:
+        <select multiple={true} value={this.state.value} onChange={this.handleChange}>
+            <option value="GM-AM-6S-GM-172_S1_L007_R1_001_report.txt">GM-AM-6S-GM-172</option>
+            <option value="GM-AM-6S-GM-174_S3_L007_R1_001_report.txt">GM-AM-6S-GM-174</option>
+            <option value="GM-AM-6S-GM-173_S2_L007_R1_001_report.txt">GM-AM-6S-GM-173</option>
+          </select>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+    
+    {this.state.data &&
+      <div>
+        <h1>Mapping</h1>
+        <div>
+          <BarChart width={800} height={400} data={this.state.data.mapping}
+                        margin={{top: 30, right: 50, left: 30, bottom: 5}}>
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip/>
+              <Legend />
+              <Bar dataKey="Total reads" fill="#a6cee3" />
+              <Bar dataKey="Mapped reads" fill="#1f78b4" />
+              <Bar dataKey="Non-redundant uniquely mapped reads" fill="#b2df8a" />
+              <Bar dataKey="Useful reads" fill="#33a02c" />
+          </BarChart>
+        </div>
+        <h1>chrM rate</h1>
+        <div>
+          <BarChart width={800} height={400} data={this.state.data['chrM rate']}
+                        margin={{top: 30, right: 50, left: 30, bottom: 5}}>
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip/>
+              <Legend />
+              <Bar dataKey="Percentage of uniquely mapped reads in chrM" fill="#a6cee3" />
+              <Bar dataKey="Percentage of reads in chrX" fill="#1f78b4" />
+          </BarChart>
+        </div>
+        <h1>Library Complexity</h1>
+        <div>
+          <BarChart width={800} height={400} data={this.state.data['library complexity']}
+                        margin={{top: 30, right: 50, left: 30, bottom: 5}}>
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip/>
+              <Legend />
+              <Bar dataKey="Before alignment library duplicates percentage" fill="#a6cee3" />
+              <Bar dataKey="After alignment PCR duplicates percentage" fill="#1f78b4" />
+          </BarChart>
+        </div>
+        <h1>Enrichment</h1>
+        <div>
+          <BarChart width={800} height={400} data={this.state.data['enrichment']}
+                        margin={{top: 30, right: 50, left: 30, bottom: 5}}>
+              <XAxis dataKey="name"/>
+              <YAxis/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip/>
+              <Legend />
+              <Bar dataKey="Useful reads ratio" fill="#a6cee3" />
+              <Bar dataKey="Percentage of background RPKM smaller than 0.3777" fill="#1f78b4" />
+          </BarChart>
+        </div>
+        <h1>Peaks</h1>
+        <div>
+          <BarChart width={800} height={400} data={this.state.data['peaks']}
+                        margin={{top: 30, right: 50, left: 30, bottom: 5}}>
+              <XAxis dataKey="name"/>
+              <YAxis yAxisId="left" orientation="left" stroke="#8884d8"/>
+              <YAxis yAxisId="right" orientation="right" stroke="#82ca9d"/>
+              <CartesianGrid strokeDasharray="3 3"/>
+              <Tooltip/>
+              <Legend />
+              <Bar yAxisId="left" dataKey="Number of peaks" fill="#8884d8" />
+              <Bar yAxisId="right" dataKey="Reads under peaks ratio" fill="#82ca9d" />
+          </BarChart>
+        </div>
+      </div>
+    }
+    
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ report-api.py
@@ -0,0 +1,80 @@
+import sys,os
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+def parse_report(f):
+    d = {}
+    with open(f) as fin:
+        for line in fin:
+            t = line.strip().split()
+            if len(t) < 4: continue
+            k = ' '.join(t[:-2])
+            v1 = t[-2]
+            v2 = t[-1].split('(')[0]
+            d[k] = [s2n(v1),s2n(v2)]
+    return d
+
+def s2n(s):
+    '''safely convert string to numbers, return int, float if float or percetage'''
+    if '%' in s:
+        return float(s.strip('%'))/100.0
+    elif '.' in s:
+        return float(s)
+    else:
+        return int(s)
+
+def main(flist):
+    d = {}
+    flis = flist.split(',')
+    #flis.sort()
+    for f in flis:
+        k = os.path.basename(f)
+        d[k] = parse_report(f)
+    #print d
+    kf = [os.path.basename(x) for x in flis]
+    #mapping
+    header = ['Total reads','Mapped reads','Non-redundant uniquely mapped reads','Useful reads']
+    header2 = ['Percentage of uniquely mapped reads in chrM','Percentage of reads in chrX','Percentage of reads in chrX']
+    header3 = ['Before alignment library duplicates percentage','After alignment PCR duplicates percentage']
+    header4 = ['Useful reads ratio','Percentage of background RPKM smaller than 0.3777']
+    header5 = ['Number of peaks','Reads under peaks ratio']
+    outheader = {
+        'mapping':header,
+        'chrM rate':header2,
+        'library complexity': header3,
+        'enrichment':header4,
+        'peaks':header5
+    }
+    results = {}
+    # the results stucture was optimized for raw d3 usage
+    # the results stucture was optimized for react-d3-components usage
+    # the results stucture was optimized for recharts usage
+    for oh in outheader:
+        results[oh] = []
+        for idxk,k in enumerate(kf):
+            td = {}
+            sampleName = k.split('_')[0]
+            td['name'] = sampleName
+            for h in outheader[oh]:    
+                td[h] = d[k][h][0]
+            results[oh].append(td)
+            if idxk == len(kf) - 1:
+                td1 = {}
+                td1['name'] = 'ENCODE'
+                for h in outheader[oh]:
+                    td1[h] = d[k][h][1]
+                results[oh].append(td1)            
+    return results
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/report/<flist>')
+def report(flist):
+    return jsonify(main(flist))
+
+
+if __name__ == "__main__":
+    main()
